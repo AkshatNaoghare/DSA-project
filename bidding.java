eecb38e0eb3132e11f96aa70fd7ca6d959687c4a
bhagyashree134340 <61799758+bhagyashree134340@users.noreply.github.com>
2020-04-24 21:33:35 +0530

    Update bidding.java

--- bidding.java
+++ bidding.java
@@ -1,9 +1,87 @@
 package carBidding;
 
-public class bidding {
 
-	public bidding() {
-		// TODO Auto-generated constructor stub
+public class bidding extends bidders
+{
+	static int round;
+	bidder h[];
+//	bidder maxBidder;
+	
+	public bidding() 
+	{
+		round = 1;
 	}
+	
+	void bidPriorityQue()
+	{
+		System.out.println("round " +round++ +" -->");
+		
+		h = list.toArray(new bidder[list.size()]);
+		
+		for(int i = 0; i<list.size(); i++)
+		{
+			System.out.print(list.get(i).name +" : ");
+			list.get(i).bid = sc.nextFloat();
+		}
+		
+		int startIdx = (list.size() / 2) - 1; // Index of last non-leaf node 
+		
+		for (int i = startIdx; i >= 0; i--) 
+		{ 
+			downAdjust(h, list.size(), i); 
+		} 
+	}
+	
+	void downAdjust(bidder array[], int size, int i) 
+	{ 
+		int largest = i;    // Initialize current node as largest 
+		int left = 2 * i + 1;   // position of left child in array = 2*i + 1 
+		int right = 2 * i + 2;   // position of right child in array = 2*i + 2 
 
+		if (left < size && array[left].bid > array[largest].bid)  // If left child is larger than root 
+			largest = left; 
+
+		if (right < size && array[right].bid > array[largest].bid) // If right child is larger than largest so far 
+			largest = right; 
+
+		if (largest != i) 
+		{         // If largest is not root swap it 
+			bidder swap = array[i]; 
+			array[i] = array[largest]; 
+			array[largest] = swap; 
+
+			downAdjust(array, size, largest); // Recursively heapify the sub-tree 
+		} 
+	} 
+	
+	void deleteRoot()
+	{
+		int n = list.size();
+		// Get the last element 
+	    bidder k = h[n - 1]; 
+	  
+	    // Replace root with first element 
+	    h[0] = k; 
+	  
+	    // Decrease size of heap by 1 
+	    n = n - 1; 
+	    
+		downAdjust(h, n, 0);
+		
+	}
+	
+	void printHeap(bidder arr[], int n) 
+	{ 
+		
+		for (int i = 0; i < n; ++i) 
+		{
+			System.out.print(arr[i].name + " "); 
+		}
+		System.out.println(); 
+	} 
+	
+	bidder maximumBid()
+	{
+		return h[0];
+	}
 }
