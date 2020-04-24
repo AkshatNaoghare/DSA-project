572b9827a262e2b5f3ae47a07e0722945601b57f
bhagyashree134340 <61799758+bhagyashree134340@users.noreply.github.com>
2020-04-24 23:02:42 +0530

    Update biddingPlatform.java

--- biddingPlatform.java
+++ biddingPlatform.java
@@ -7,9 +7,39 @@ public class biddingPlatform extends bidding
 
 	}
 	
-	void maxBidderSelect()
+	void rounds()
 	{
+		char ch;
+		
 		bidder maxBidder = maximumBid();
+		
+		System.out.println("continue rounds? (y/n)");
+		
+		if(sc.next().charAt(0) == 'y')
+		{
+			bidPriorityQue();
+		}
+		else
+		{
+			sellCar(maxBidder);
+			deleteBidder();
+			printHeap(h, list.size()-1);
+		}
+	}
+	
+	void sellCar(bidder maxBidder)
+	{
+		System.out.println(maxBidder.bid);
+		System.out.println(maximumBid().bid);
+		
+		if(maxBidder.bid >= maximumBid().bid)
+		{
+			System.out.println("car is sold to " +maxBidder.name);
+		}
+		else
+		{
+			System.out.println("car is sold to " +maximumBid().name);
+		}
 	}
 
 }
