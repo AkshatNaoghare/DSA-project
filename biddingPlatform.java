f62efd7688907f6e28b6e8bcbdfa9d6933eee0b0
Akshat Naoghare <55185050+AkshatNaoghare@users.noreply.github.com>
2020-04-25 01:52:00 +0530

    Update biddingPlatform.java

--- biddingPlatform.java
+++ biddingPlatform.java
@@ -7,7 +7,7 @@ public class biddingPlatform extends bidding
 
 	}
 	
-	void rounds()
+	void rounds(car c)
 	{
 		char ch;
 		
@@ -21,13 +21,13 @@ public class biddingPlatform extends bidding
 		}
 		else
 		{
-			sellCar(maxBidder);
+			sellCar(maxBidder,c);
 			deleteBidder();
 			printHeap(h, list.size()-1);
 		}
 	}
 	
-	void sellCar(bidder maxBidder)
+	void sellCar(bidder maxBidder, car c)
 	{
 		System.out.println(maxBidder.bid);
 		System.out.println(maximumBid().bid);
@@ -35,10 +35,14 @@ public class biddingPlatform extends bidding
 		if(maxBidder.bid >= maximumBid().bid)
 		{
 			System.out.println("car is sold to " +maxBidder.name);
+			c.buyer=maxBidder.name;
+			c.soldprice=maxBidder.bid;
 		}
 		else
 		{
 			System.out.println("car is sold to " +maximumBid().name);
+			c.buyer=maximumBid().name;
+			c.soldprice=maximumBid().bid;
 		}
 	}
 
