8b5d2c46609d594d94f7e898135998290c89423f
bhagyashree134340 <61799758+bhagyashree134340@users.noreply.github.com>
2020-04-25 18:27:37 +0530

    Update carBidding.java

--- carBidding.java
+++ carBidding.java
@@ -11,30 +11,31 @@ public class carBidding
 	
 	public static void main(String[] args) 
 	{
-		/*biddingPlatform b = new biddingPlatform();
-		b.create();
-		b.create();
-		b.create();
-		b.create();
-		b.create();
-//		System.out.println(b.search().name);
-		b.waitingList(b.root);
-		b.display();
-//		System.out.println("delete");
-//		b.delete(b.search());
+//		biddingPlatform b = new biddingPlatform();
+//		b.create();
+//		b.create();
+//		b.create();
+//		b.create();
+//		b.create();
+////		System.out.println(b.search().name);
+//		b.waitingList(b.root);
 //		b.display();
-		b.bidPriorityQue();
-//		System.out.println(b.h[0].name);
-		b.printHeap(b.h, b.list.size());
-		b.rounds();
-		b.rounds();*/
+////		System.out.println("delete");
+////		b.delete(b.search());
+////		b.display();
+//		b.bidPriorityQue();
+////		System.out.println(b.h[0].name);
+//		b.printHeap(b.h, b.list.size());
+//		b.rounds();
+//		b.rounds();
 		
 		cars cs=new cars();		//object for cars class
-		bidders bs=new bidders();
+		biddingPlatform b = new biddingPlatform();
 		
 		Scanner sc =new Scanner(System.in);
 		
 		int ch;
+		char ch1;
 		int nc=0;			//no. of cars registered
 		int nb=0;			//no. of bidders registered
 		
@@ -42,50 +43,72 @@ public class carBidding
 		
 		do
 		{
-			System.out.println("\nMake entry for 1.Car seller OR 2. Bidder? ");
-			System.out.println("Enter 0 if you are done with all entries.");
-		
-			ch=sc.nextInt();
+			System.out.println("\na.enter details to participate in the bidding"
+					+ "\nb.start bidding\nc.exit");
+			ch1 = sc.next().charAt(0);
 			
-			switch(ch)
+			switch(ch1)
 			{
-			case 0:
-				//All entries accepted, move to the bids
-				break;
-				
-			case 1:
-				nc++;
-				cs.insert();
-				System.out.println("THANK-YOU!");
-				break;
-				
-			case 2:
-				nb++;
-				//Accept details of bidder
-				bs.create();
-				System.out.println("THANK-YOU!");
-				break;
-				
-				default:
-					System.out.println("ENTER VALID OPTION!");
+				case 'a':
+					do
+					{
+						System.out.println("\nMake entry for 1.Car seller OR 2. Bidder? ");
+						System.out.println("Enter 0 - back.");
+					
+						ch=sc.nextInt();
+						
+						switch(ch)
+						{
+							
+							case 1:
+								nc++;
+								cs.insert();
+								System.out.println("THANK-YOU!");
+								break;
+								
+							case 2:
+								nb++;
+								//Accept details of bidder
+								b.create();
+								System.out.println("THANK-YOU!");
+								break;
+								
+
+						}
+					}while(ch!=0);
+					
+					break;
+					
+				case 'b':
+					
+					System.out.println("\nLET'S BEGIN WITH THE BID ROUNDS! GET YOUR BIDS IN!");
+					
+					for(int i=0; i<nc; i++)
+					{
+						System.out.println("\nCAR No. -->"+(i+1));
+						car c=cs.select();
+						
+						cs.display(c);
+						
+					}
+					
+					break;
+					
+				case 'c':
+					
+					break;	
 			}
-		}while(ch!=0);
 		
-		System.out.println("\nLET'S BEGIN WITH THE BID ROUNDS! GET YOUR BIDS IN!");
+		}while(ch1 != 'c');
 		
-		for(int i=0; i<nc; i++)
-		{
-			System.out.println("\nCAR No. -->"+(i+1));
-			car c=cs.select();
-			
-			cs.display(c);
-			//bidpriority queue //rounds(c)
-			
-			
-			//bid rounds
-		}
 		
+		
+		
+		
+		cs.index = 0;
 		car cc;
+		
+		System.out.println("results of all cars: ");
 		for(int i=0; i<nc; i++)				//print all car details including buyer after all rounds
 		{
 			System.out.println("\nResults of all cars:");
