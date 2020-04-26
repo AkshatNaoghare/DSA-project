f44edd9cb082f12ba3e76061a8c9c586fef6f438
Akshat Naoghare <55185050+AkshatNaoghare@users.noreply.github.com>
2020-04-26 20:46:49 +0530

    Update biddingPlatform.java

--- biddingPlatform.java
+++ biddingPlatform.java
@@ -1,50 +1,92 @@
 package carBidding;
 
+//import java.util.ArrayList;
+
 public class biddingPlatform extends bidding
 {
+	int r;			//for validating when all bidders enter their bid as 0
 	public biddingPlatform() 
 	{
-
+		r=0;
 	}
 	
-	void rounds()
+	void rounds(car c)
 	{
-		char ch;
+	//	char ch;
 		
 		bidder maxBidder = maximumBid();
 		
-		if(round <= 3)
+		printHeap(h, list.size());
+		
+		if((maxBidder!=null)&&(maxBidder.bid==0))
 		{
-			System.out.println("continue rounds? (y/n)");
-			
-			if(sc.next().charAt(0) == 'y')
+			if(list.size()==0)
 			{
-				bidPriorityQue();
+				
 			}
 			else
 			{
-				sellCar(maxBidder);
-				deleteBidder();
-//				printHeap(h, list.size()-1);
+			r=1;
+			round=1;
+			System.out.println("Car sold to no one since all entered their bids as 0");
 			}
 		}
+		
+		/*System.out.println("continue rounds? (y/n)");
+		
+		if(sc.next().charAt(0) == 'y')
+		{
+			bidPriorityQue(c);
+		}
+		else
+		{
+			sellCar(maxBidder,c);
+			deleteBidder();
+			printHeap(h, list.size()-1);
+		}*/
 		
 		
 	}
 	
-	void sellCar(bidder maxBidder)
+	void sellCar(bidder maxBidder, car c)
 	{
-		System.out.println(maxBidder.bid);
-		System.out.println(maximumBid().bid);
-		
-		if(maxBidder.bid >= maximumBid().bid)
+		/*System.out.println(maxBidder.bid);
+		System.out.println(maximumBid().bid);*/
+		if(t==0)
 		{
-			System.out.println("car is sold to " +maxBidder.name);
+			if(maxBidder.bid >= maximumBid().bid)
+			{
+				System.out.println("Car is sold to: " +maxBidder.name);
+				maxBidder.budget=maxBidder.budget-maxBidder.bid;
+				System.out.println("Remaining budget: "+maxBidder.budget);
+				c.buyer=maxBidder.name;
+				c.soldprice=maxBidder.bid;
+			}
+			else
+			{
+				System.out.println("Car is sold to: " +maximumBid().name);
+				maximumBid().budget=maximumBid().budget-maximumBid().bid;
+				System.out.println("Remaining budget: "+maximumBid().budget);
+	
+				c.buyer=maximumBid().name;
+				c.soldprice=maximumBid().bid;
+			}
+		}
+		else if(maximumBid()==null)
+		{
+			
 		}
 		else
 		{
-			System.out.println("car is sold to " +maximumBid().name);
+			System.out.println("Car is sold to: " +maximumBid().name);
+			maximumBid().budget=maximumBid().budget-maximumBid().bid;
+			System.out.println("Remaining budget: "+maximumBid().budget);
+
+			c.buyer=maximumBid().name;
+			c.soldprice=maximumBid().bid;
 		}
+		round=1;
+		h=null;
 	}
 
 }
