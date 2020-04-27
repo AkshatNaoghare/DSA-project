a458b7279ef0a2d953d62cff73725b310c0335ed
bhagyashree134340 <61799758+bhagyashree134340@users.noreply.github.com>
2020-04-27 13:17:26 +0530

    Update cars.java

--- cars.java
+++ cars.java
@@ -6,13 +6,14 @@ import java.util.regex.Pattern;
 
 public class cars 
 {
-	car select;
-	static int index;
-	int max;
-	car carStore[];
+	car select;		//Selects a car from the hash
+	static int index;		//Address of the hash
+	int max;		// Stores size of the hash
+	car carStore[];		// Stores the hash
+	
 	Scanner sc=new Scanner(System.in);
 	
-	public cars() 
+	public cars()		// constructor of class cars 
 	{
 		index=0;
 		max=15;
@@ -20,7 +21,7 @@ public class cars
 		select=carStore[index];
 	}
 	
-	void insert()
+	void insert()		// inserts entries in the hash
 	{
 		String name;
 		String num;	
@@ -31,43 +32,54 @@ public class cars
 		int hash_add;
 		
 		
-		System.out.println("Enter car Owner Name: ");
+		System.out.print("\n\t\tEnter car Owner Name: \t\t");
 		name=sc.next();
+		System.out.println();
 		
 		int flag=0;
-		System.out.println("Enter license plate number of the car: (LL-XXX-LLL-XXXX)");
+		
 		do
 		{
 			flag=0;
+			
+			System.out.print("\n\t\tEnter license plate number of the car: (LL-XX-LL-XXXX)\t\t");
 			num=sc.next();
+			System.out.println();
 			
 			if(validator(num))
 			{
 			}
 			else
 			{
-				System.out.println("Invalid format of License plate number! Enter in (LL-XXX-LLL-XXXX) format:");
+				System.out.println("\n\t\tInvalid format of License plate number! Enter in (LL-XX-LLL-XXX) format:");
 				flag=1;
 			}
 			
 			if(search(num))
 			{
-				System.out.println("Duplicate license plate number!");
-				System.out.println("Re-enter!");
+				System.out.println("\n\t\tDuplicate license plate number!");
+				System.out.println("\n\t\tRe-enter!");
 				flag=1;
 			}
 			
 			
-		}while(flag==1);
+		}while(flag == 1);
 
-		System.out.println("Enter car brand: ");
+		System.out.print("\n\t\tEnter car brand: \t\t");
 		brand=sc.next();
-		System.out.println("Enter car model name: ");
+		System.out.println();
+		
+		System.out.print("\n\t\tEnter car model name: \t\t");
 		model=sc.next();
-		System.out.println("Enter original cost of the car: ");
+		System.out.println();
+		
+		System.out.print("\n\t\tEnter original cost of the car: \t\t");
 		cost=sc.nextFloat();
-		System.out.println("Enter minimum bid expectes by the owner for this car: ");
+		System.out.println();
+		
+		System.out.print("\n\t\tEnter minimum bid expectes by the owner for this car: \t\t");
 		bid=sc.nextFloat();
+		System.out.println();
 		
 		car temp=new car(name,num,brand,model,cost,bid);
 		
@@ -99,14 +111,16 @@ public class cars
 		
 		if(ptr.soldprice==0)
 		{
-			System.out.println("\tOriginal cost of the car-->"+ptr.originalCost);
-			System.out.println("\tMinimum bid expected by owner-->"+ptr.minBid);
+			System.out.println("\tOriginal cost of the car --> $"+ptr.originalCost);
+			System.out.println("\tMinimum bid expected by owner --> $"+ptr.minBid);
 		}
 		else
 		{
-			System.out.println("\tCar sold at a price of-->"+ptr.soldprice);
+			System.out.println("\tCar sold at a price of --> $"+ptr.soldprice);
 			System.out.println("\tTo-->"+ptr.buyer);
 		}
+		
+		System.out.println("\t-----------------------------");
 		
 	}
 	
@@ -119,57 +133,62 @@ public class cars
 			ptr=carStore[i];
 			while(ptr!=null)
 			{
-				System.out.println("\n\tCar Brand name: "+ptr.brand);
-				System.out.println("\tCar Model Name: "+ptr.modelName);
-				System.out.println("\tCar Owner name: "+ptr.name);
-				System.out.println("\tCar License Plate Number: "+ptr.licPNo);
+				System.out.println("\t\tCar Brand name: "+ptr.brand);
+				System.out.println("\t\tCar Model Name: "+ptr.modelName);
+				System.out.println("\t\tCar Owner name: "+ptr.name);
+				System.out.println("\t\tCar License Plate Number: "+ptr.licPNo);
 				
 				if(ptr.soldprice==0)
 				{
-					System.out.println("\tCar Unsold!");
+					System.out.println("\n\t\tCar Not Sold Yet!");
 				}
 				else
 				{
-					System.out.println("\tCar sold at a price of-->"+ptr.soldprice);
-					System.out.println("\tTo-->"+ptr.buyer);
+					System.out.println("\n\t\tCar sold at a price of --> $"+ptr.soldprice);
+					System.out.println("\n\t\tTo --> "+ptr.buyer);
 				}
 				ptr=ptr.next;
+				System.out.println("\t\t-----------------------------");
 			}
 		}
 	}
 	
 	boolean search(String str)		//str is license plate number of the car which is dequeued
 	{
-		int flag=0;
+		int flag = 0;
 		
-		int key=Integer.parseInt(str.substring(11));
-		int addr=(int)(key%max);
-		
-		car ptr=carStore[addr];
-		while(ptr!=null)
+		if(validator(str))
 		{
-
-			if(ptr.licPNo.equals(str))
+			int key=Integer.parseInt(str.substring(9, 13));
+			int addr=(int)(key%max);
+			
+			car ptr=carStore[addr];
+			
+			while(ptr!=null)
 			{
-				flag=1;
-				break;
+	
+				if(ptr.licPNo.equals(str))
+				{
+					flag=1;
+					break;
+				}
+				ptr=ptr.next;
 			}
-			ptr=ptr.next;
 		}
-		
-		if(flag==1)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+			
+			if(flag==1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 	}
 	
 	void delete(String str)
 	{
-		int key=Integer.parseInt(str.substring(6,10));
+		int key=Integer.parseInt(str.substring(8,13));
 		int addr=(int)(key%max);
 		
 		car ptr=carStore[addr].next;
@@ -201,47 +220,49 @@ public class cars
 		}
 	}
 	
-	car select()
+	car select()		// Selects a car node sequentially every time select() is called 
 	{
 		int flag=0;
 		car current=null;/*
 		select=carStore[index];*/
-	do {
-		flag=0;
-		while(select==null)			//if the node in the hash table is empty(null)
-	
-		{
-			index++;//inc head
-			if(index==15)
-			{
-				index=0;
-			}
-			select=carStore[index];		//we move to next head
-		}
 		
-		if(select!=null)
+		do 
 		{
-			if(!select.sold)
-			{	current=select;
-				select=select.next;
-				if(select==null)
-				{
-					break;
-				}
-			}else
+			flag=0;
+			while(select==null)			//if the node in the hash table is empty(null)
+		
 			{
-				while(select.sold)
+				index++;//inc head
+				if(index==15)
 				{
-					if(select.next==null)
+					index=0;
+				}
+				select=carStore[index];		//we move to next head
+			}
+			
+			if(select!=null)
+			{
+				if(!select.sold)
+				{	current=select;
+					select=select.next;
+					if(select==null)
 					{
-						flag=1;
-						select=select.next;
 						break;
 					}
-					select=select.next;
+				}else
+				{
+					while(select.sold)
+					{
+						if(select.next==null)
+						{
+							flag=1;
+							select=select.next;
+							break;
+						}
+						select=select.next;
+					}
+					current=select;
 				}
-				current=select;
-			}
 		}
 	}while(flag==1);
 	return current;
@@ -294,7 +315,7 @@ public class cars
 		}*/
 	}
 	
-	boolean validator(String lpn)
+	boolean validator(String lpn)		// Validates the entered string as car license plate number
 	{
 		int len = lpn.length();
 		boolean validLength = len>=10;
